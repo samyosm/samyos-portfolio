9721833c73c6c262c5574242cb52089629c4d0a3
Samy Rahmani <samy.osmium@gmail.com>
2022-12-30 15:54:10 -0500

    Added ApolloClient

---
+++ frontend/contexts/ApolloClient.ts
@@ -0,0 +1,7 @@
+import {ApolloClient, InMemoryCache} from "@apollo/client";
+
+
+export const apolloClient = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
