b0efc4fb7fbdb00862f0fbe701fecc87a5ccc2be
Samy Rahmani <samy.osmium@gmail.com>
2022-12-30 15:57:52 -0500

    Added .env and set up ApolloClient

--- frontend/contexts/ApolloClient.ts
+++ frontend/contexts/ApolloClient.ts
@@ -2,6 +2,6 @@ import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 
 export const apolloClient = new ApolloClient({
-  uri: 'https://flyby-gateway.herokuapp.com/',
+  uri: process.env.STRAPI_GQL_LINK,
   cache: new InMemoryCache(),
 });
